f4903dab3db06c5685640d30f153ab40e7a89a98
Milanz505 <gabrielmilanz00@gmail.com>
2026-06-05 18:30:34 -0300

    test: add controller coverage for image endpoint

--- src/test/java/com/frivclone/controllers/GameControllerTest.java
+++ src/test/java/com/frivclone/controllers/GameControllerTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -130,5 +131,12 @@ class GameControllerTest {
     void shouldReturn404WhenRequestingNonExistentImage() throws Exception {
         mockMvc.perform(get("/api/games/images/games/doesnotexist.jpg"))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldServeExistingImageWithDetectedContentType() throws Exception {
+        mockMvc.perform(get("/api/games/images/games/pacman.webp"))
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "image/webp"));
     }
 }
